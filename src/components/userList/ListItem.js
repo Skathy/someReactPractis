b5fcc40cc453e922657159215100dfa57785f2e1
oleg <chibikstrong@gmail.com>
2020-08-09 13:52:48 +0300

    somestyles

--- src/components/userList/ListItem.js
+++ src/components/userList/ListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
+//import './userListStyle.css';
 
 const ListItem = (props) => {
   const {
+    isSelected,
     user: { id, fname, lname },
   } = props;
-
+  const styles = { color: `rgb(255, 0, 0)` };
   return (
-    <li>
+    <li style={styles}>
       UserID: "{id}"; UserFullName:"{fname} {lname}";
     </li>
   );
